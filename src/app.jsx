8c9e9a6d82481e9484feab54d1f411e5f89b5d35
Sonya Shakirova <smshakirova@mail.ru>
2023-07-01 16:31:20 +0300

    feat: add useFetch hook

--- src/app.jsx
+++ src/app.jsx
@@ -1,3 +1,27 @@
+import { useFetch } from "./hooks/useFetch"
+
 export function App() {
-  return <></>
+  const {
+    data,
+    isLoading,
+    error,
+    refetch
+  } = useFetch("https://jsonplaceholder.typicode.com/posts")
+	
+  return (
+    <div>
+      <div>
+        <button onClick={() => refetch({
+          params: {
+            _limit: 3
+          }
+        })}>
+          Перезапросить
+        </button>
+      </div>
+      {isLoading && "Загрузка..."}
+      {error && "Произошла ошибка"}
+      {data && !isLoading && data.map(item => <div key={item.id}>{item.title}</div>) }
+    </div>
+  )
 }
---
+++ src/hooks/useFetch.js
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react"
+
+export function useFetch(url) {
+  const [data, setData] = useState([])
+  const [isLoading, setLoading] = useState(true)
+  const [error, setError] = useState(false)
+
+  const fetchData = async (url, params) => {
+    setLoading(true)
+    
+    const response = await fetch(
+      params ? `${url}?${new URLSearchParams(params)}` : url
+    )
+
+    if (response.ok) {
+      const json = await response.json()
+      setData(json)
+    } else {
+      setError(true)
+    }
+
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    fetchData(url)
+  }, [])
+
+  async function refetch({ params }) {
+    fetchData(url, params)
+  }
+
+  return {
+    data,
+    isLoading,
+    error,
+    refetch
+  }
+}
